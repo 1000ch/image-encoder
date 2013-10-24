1206f8823ef1331ce3030d48cb62371ded64f1b5
1000ch <orz1000ch@gmail.com>
2013-10-24 20:07:21 +0900

    add comments

--- src/ImageEncoder.js
+++ src/ImageEncoder.js
@@ -1,31 +1,54 @@
 (function(window) {
-  
+
+  /**
+   * ImageEncoder constructor
+   * @param {String} path
+   * @constructor
+   */
   function ImageEncoder(path) {
-    this.file = path ? path : "";
+    this.filePath = path ? path : "";
     this.canvas = document.createElement('canvas');
     this.image = null;
     this.width = null;
     this.height = null;
   }
-  
+
+  /**
+   * Set resource path
+   * @param {String} path
+   */
   ImageEncoder.prototype.setPath = function(path) {
-    this.file = path;
+    this.filePath = path;
   };
-  
+
+  /**
+   * Set size
+   * @param {Number} width
+   * @param {Number} height
+   */
   ImageEncoder.prototype.setSize = function(width, height) {
     this.width = width;
     this.height = height;
   };
 
+  /**
+   * Release memory
+   */
   ImageEncoder.prototype.dispose = function() {
     this.canvas = null;
     this.image = null;
   };
-  
+
+  /**
+   * Generate datauri
+   * @param {Function} callback
+   */
   ImageEncoder.prototype.getDataURI = function(callback) {
+    if(!this.filePath) {
+      return null;
+    }
     var that = this;
-    this.image = new Image();
-    this.image.onload = function() {
+    var onloadCallback = function(e) {
       that.canvas.width = that.image.width;
       that.canvas.height = that.image.height;
       if(that.width) {
@@ -37,8 +60,11 @@
       var context = that.canvas.getContext("2d");
       context.drawImage(that.image, 0, 0, that.canvas.width, that.canvas.height);
       callback(that.canvas.toDataURL("image/png", 1));
+      that.image.removeEventListener("load", onloadCallback);
     };
-    this.image.src = this.file;
+    this.image = new Image();
+    this.image.addEventListener("load", onloadCallback);
+    this.image.src = this.filePath;
   };
   
   window.ImageEncoder = ImageEncoder;
